a356c410ad2d89edef9c035d325c1e51fdf4c540
carlos-eduardo-ip <carlos.eduardog2p@gmail.com>
2024-06-16 23:56:12 -0300

    Criando um APP no Django. Adicionando urls e configurando as views.

---
+++ CursoDjango/recipes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+#HTTP REQUEST
+
+def home(request):
+    return HttpResponse('HOME')
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
