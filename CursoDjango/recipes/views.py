56f0d39d2b5d7bf19e454b333c155e07ab268513
carlos-eduardo-ip <carlos.eduardog2p@gmail.com>
2024-07-07 21:59:06 -0300

    Injetando dados fake para visualização dos cards na Home.

--- CursoDjango/recipes/views.py
+++ CursoDjango/recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
@@ -7,10 +8,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'recipes/pages/home.html', context={
-        'name': 'Carlos Eduardo'
+        'recipes': [make_recipe() for _ in range(10)]
     })
 
 def recipe(request, id):
     return render(request,'recipes/pages/recipe-view.html', context={
-        'name': 'Carlos Eduardo'
+        'recipe': make_recipe()
     })
